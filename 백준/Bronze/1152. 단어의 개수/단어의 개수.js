32e70f9fb4f4579cd3444dd158eaf921af4799af
zx3923 <109199190+zx3923@users.noreply.github.com>
2024-07-09 19:49:34 +0900

    [Bronze II] Title: 단어의 개수, Time: 152 ms, Memory: 16880 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.js
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.js
@@ -1,4 +1,8 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim();
-const words = input.split(" ").filter(word => word !== "");
-console.log(words.length);
+if (input === "") {
+  console.log(0);
+} else {
+  const words = input.split(" ");
+  console.log(words.length);
+}
