87029964ffe5c9d1ef363492240bbe7c6277f26d
zx3923 <109199190+zx3923@users.noreply.github.com>
2024-12-15 19:11:39 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 3840 ms, Memory: 62796 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin", "utf8").trim().split("\n");
+const n = parseInt(input[0]);
+
+const points = input.slice(1).map((line) => line.split(" ").map(Number));
+
+points.sort((a, b) => {
+  if (a[1] === b[1]) {
+    return a[0] - b[0];
+  } else {
+    return a[1] - b[1];
+  }
+});
+
+for (let i = 0; i < n; i++) {
+  console.log(points[i].join(" "));
+}
